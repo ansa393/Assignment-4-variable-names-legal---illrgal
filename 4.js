257dbf56e596e8100cf0f96eb3b3cc7e18237f5a
Ansa Ghani <ansaghani74@gmail.com>
2025-07-07 11:54:05 -0700

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,11 @@
+let rules = ["<h1>", "Rules for naming JS variables" , "<br>" , "</h1>"]
+document.write(rules)
+
+let variable = ["Variable names can only contain, numbers, $and. For example: $my_1stVariable" , "<br>" ];
+document.write(variable)
+let example = ["Variable must begin with a letter, Sor. For example: Sname, name or name", "<br>"];
+document.write(example)
+let sensitive = ["Variable names are case sensitive"  , "<br>"];
+document.write(sensitive)
+let should = ["Variable names should not be JS keywords" , "<br>"]
+document.write(should)
